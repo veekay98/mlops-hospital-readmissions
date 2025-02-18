ae1e5c09b96a3c5daa15a3b0940dedbf9a067460
veekay98 <veekay98@outlook.com>
2025-02-18 12:42:43 -0500

    Fixing the payload

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,24 @@ class TestFastAPI(unittest.TestCase):
 
     def test_prediction_endpoint():
         url = "http://127.0.0.1:7500/predict"
-        payload = {"some": "data"}  # Make sure this matches your expected input format
+        payload = {
+                      "age": 45,
+                      "time_in_hospital": 5,
+                      "n_lab_procedures": 30,
+                      "n_procedures": 2,
+                      "n_medications": 10,
+                      "n_outpatient": 1,
+                      "n_inpatient": 0,
+                      "n_emergency": 0,
+                      "medical_specialty": "Endocrinology",
+                      "diag_1": "250.02",
+                      "diag_2": "401.1",
+                      "diag_3": "E11.9",
+                      "glucose_test": 140,
+                      "A1Ctest": 6.5,
+                      "change": "Yes",
+                      "diabetes_med": "Metformin"
+                    }  # Make sure this matches your expected input format
 
         response = requests.post(url, json=payload)
 
